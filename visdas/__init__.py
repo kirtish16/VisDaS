9e3bc2ea3d55b16c2656d6acbc729aae4cf84d85
kirtish16 <kirtish16@gmail.com>
2021-09-22 23:30:32 +0530

    Initial commit

---
+++ visdas/__init__.py
@@ -0,0 +1,10 @@
+from .array_visualizer import Array
+
+# arr = Array()
+# for i in list([11,2223,11,12345]):
+#     arr.insert(i)
+
+# arr.remove(11)
+# # arr.remove(10)
+# print(arr.array)
+# arr.generate()
